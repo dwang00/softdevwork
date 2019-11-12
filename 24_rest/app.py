48005e241e2ce2c33cc43f9e8cb10e97b5092831
David Wang <dwang00@stuy.edu>
2019-11-12 10:07:26 -0500

    added

---
+++ 24_rest/app.py
@@ -0,0 +1,23 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev1 pd0
+#demo -- My First Flask App
+#2019-09-17t
+
+from flask import Flask, render_template
+import urllib, json
+app = Flask(__name__) #create instance of class Flask
+
+@app.route("/") #assign following fxn to run when root route requested
+def root():
+    u = urllib.request.urlopen(
+    "https://api.nasa.gov/planetary/apod?api_key=fQ5u02rjFLFPZQbMTdtwNbjoZnLH6FnBrkV8v450"
+    )
+    response = u.read()
+    data = json.loads(response)
+    return render_template("index.html", pic = data['url'])
+    print(__name__) #where will this go?
+    return "No hablo queso!"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
